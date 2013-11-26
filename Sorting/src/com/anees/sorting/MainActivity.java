9a3a48034d0d2f1b0696d16bd9a654595e8be84a
sakzk007 <sakzk007@gmail.com>
2013-11-26 23:09:36 +0530

    Improved coding using custom formatter.

--- Sorting/src/com/anees/sorting/MainActivity.java
+++ Sorting/src/com/anees/sorting/MainActivity.java
@@ -12,76 +12,71 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-	Button sort;
-	EditText text;
-	TextView tvResult;
-	String ss;
-	int a[];
+    Button sort;
+    EditText text;
+    TextView tvResult;
+    String ss;
+    int a[];
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		initVars();
-		sort.setOnClickListener(new View.OnClickListener() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
+	initVars();
+	sort.setOnClickListener(new View.OnClickListener() {
 
-			@Override
-			public void onClick(View arg0) {
-				tvResult.setText("");
-				ss = text.getText().toString().trim();
-				if (ss.length() != 0 && ss != null) {
-					performParsingAndSorting();
-				}
+	    @Override
+	    public void onClick(View arg0) {
+		tvResult.setText("");
+		ss = text.getText().toString().trim();
+		if (ss.length() != 0 && ss != null) {
+		    performParsingAndSorting();
+		}
+	    }
+
+	    private void performParsingAndSorting() {
+		String[] strInts = ss.split(",");
+		a = new int[strInts.length];
+		Log.d("Lengths", "Str Length : " + strInts.length + "and Int Length: " + a.length);
+		boolean flag = true;
+		for (int i = 0; i < strInts.length; i++) {
+		    try {
+			a[i] = Integer.parseInt(strInts[i]);
+		    } catch (Exception e) {
+			flag = false;
+			e.printStackTrace();
+		    }
+
+		}
+		if (flag) {
+		    // Perform Sort Here
+		    /*
+		     * Arrays.sort(a);
+		     */
+		    for (int i = 0; i < a.length; i++) {
+			for (int j = i; j < a.length; j++) {
+			    if (a[i] > a[j]) {
+				a[i] = a[i] + a[j];
+				a[j] = a[i] - a[j];
+				a[i] = a[i] - a[j];
+			    }
 			}
+		    }
 
-			private void performParsingAndSorting() {
-				String[] strInts = ss.split(",");
-				a = new int[strInts.length];
-				Log.d("Lengths","Str Length : " + strInts.length
-						+ "and Int Length: " + a.length);
-				boolean flag = true;
-				for (int i = 0; i < strInts.length; i++) {
-					try {
-						a[i] = Integer.parseInt(strInts[i]);
-					} catch (Exception e) {
-						flag = false;
-						e.printStackTrace();
-					}
+		    Log.d("output:", Arrays.toString(a));
+		    tvResult.setText(Arrays.toString(a));
 
-				}
-				if (flag) {
-					// Perform Sort Here
-					
-					/*
-					 * Arrays.sort(a);
-					*/
+		} else {
+		    Toast.makeText(getApplicationContext(), "Input Contains Invalid Integers!", Toast.LENGTH_SHORT).show();
+		}
 
-					for(int i=0;i<a.length;i++){
-						for(int j=i;j<a.length;j++){
-							if(a[i]>a[j]){
-								a[i] = a[i] + a[j];
-								a[j] = a[i] - a[j];
-								a[i] = a[i] - a[j];
-							}
-						}
-					}
-					
-					Log.d("output:",Arrays.toString(a));
-					tvResult.setText(Arrays.toString(a));
-					
-				} else {
-					Toast.makeText(getApplicationContext(),
-							"Input Contains Invalid Integers!",
-							Toast.LENGTH_SHORT).show();
-				}
+	    }
+	});
+    }
 
-			}
-		});
-	}
-
-	private void initVars() {
-		text = (EditText) findViewById(R.id.etNumbers);
-		sort = (Button) findViewById(R.id.btnSort);
-		tvResult = (TextView) findViewById(R.id.tvResult);
-	}
+    private void initVars() {
+	text = (EditText) findViewById(R.id.etNumbers);
+	sort = (Button) findViewById(R.id.btnSort);
+	tvResult = (TextView) findViewById(R.id.tvResult);
+    }
 }
